f15a13273dbda6dbd38e7971c8bea43168e4157d
David Wu <davidcnwu@gmail.com>
2012-10-28 13:34:44 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhaiku::Application.config.secret_token = 'd6a34432d8403360f43fd0582f4e9985d401542f6b7c9cec0c535290ba62218f80a0319f2036f54e3f40f957efc1eda2643516f4048fa3e9dfdd24e1b2d7ee50'
