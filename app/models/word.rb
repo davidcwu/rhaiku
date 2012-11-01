fb716ed88a41080721cbc18e37481d9c0c027933
David Wu <davidcnwu@gmail.com>
2012-10-31 21:02:40 -0700

    Adding validation checks and tests for Word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,13 @@
 class Word < ActiveRecord::Base
   attr_accessible :syllables, :word
+
+  validates :word, :uniqueness => true,
+                   :format => /\A[a-zA-z]+\z/,
+                   :presence => true
+
+  validates :syllables, :presence => true,
+                        :numericality => {
+                          :greater_than => 0,
+                          :less_than => 8
+                        }
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -14,4 +14,52 @@ describe Word do
   it "should respond to syllables" do 
     @word.should respond_to(:syllables)
   end
+
+  describe "word field" do
+
+    it "can never be nil" do
+      bad_word_entry = Word.new(@attr.merge( :word => nil ))
+      bad_word_entry.should_not be_valid
+    end
+
+    it "must be unique" do
+      first_word_entry = @word.save
+      duplicate_word_entry = Word.new(@attr)
+      duplicate_word_entry.should_not be_valid
+    end
+
+    it "cannot contain numbers" do
+      bad_word_entry = Word.new(@attr.merge( :word => 'na9' ))
+      bad_word_entry.should_not be_valid
+    end
+    
+    it "cannot contain special characters" do
+      bad_word_entry = Word.new(@attr.merge( :word => 'super8' ))
+      bad_word_entry.should_not be_valid
+    end
+
+    it "cannot contain spaces" do
+      bad_word_entry = Word.new(@attr.merge( :word => 'super mang' ))
+      bad_word_entry.should_not be_valid
+    end
+  end
+
+  describe "syllables field" do
+    it "can never be nil" do
+      bad_word_entry = Word.new(@attr.merge( :syllables => nil ))
+      bad_word_entry.should_not be_valid
+    end
+
+    it "must be a positive number" do
+      bad_word_entry = Word.new(@attr.merge( :syllables => 0 ))
+      bad_word_entry.should_not be_valid
+    end
+
+    it "must be less than 8" do
+      bad_word_entry = Word.new(@attr.merge( :syllables => 8 ))
+      bad_word_entry.should_not be_valid
+    end
+
+  end
+
 end
