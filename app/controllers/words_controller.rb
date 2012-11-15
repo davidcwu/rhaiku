1cc23a2a97bace8fd733944f9261f4e2a343a2bc
David Wu <davidcnwu@gmail.com>
2012-11-15 00:14:52 -0800

    Create and New and partials for Words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,4 +6,15 @@ class WordsController < ApplicationController
   def new
     @word = Word.new
   end
+
+  def create
+    @word = Word.new(params[:word])
+    if @word.save
+      flash[:success] = "Success!"
+      redirect_to(words_path)
+    else
+      @title = 'Create Word'
+      render :new
+    end
+  end
 end
--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -43,4 +43,41 @@ describe WordsController do
       it { should be_success }
     end
   end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :word => '', :syllables => 0 }
+      end
+
+      it 'should not create a word' do
+        expect {
+          post :create, :word => @attr
+        }.to_not change(Word, :count)
+      end
+      
+      it "should render the 'new' template" do
+        post :create, :word => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe 'success' do
+      before(:each) do
+        @attr = { :word => 'david', :syllables => 2 }
+      end
+      
+      it 'should create a word' do
+        expect {
+          post :create, :word => @attr
+        }.to change(Word, :count).by(1)
+      end
+
+      it 'should redirect to the index' do
+        post :create, :word => @attr
+        response.should redirect_to(words_path)
+      end
+    end
+
+  end
 end
